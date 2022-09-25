774e49ff8d308e5080efe8692b4bcb1da890ad95
Carlos Armas <opcnotfound@gmail.com>
2022-09-25 18:15:49 -0300

    Avance

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "../app/Provider";
 
 function CartWidget() {
-  const [cartCounter, setCartCounter] = useState(0);
+  const [cartstate, setcartState] = useContext(AppContext);
+
+  const [widgetNumber, setwidgetNumber] = useState(0);
+
+  const actNumber = () => {
+    const actualizar = cartstate.length;
+    setwidgetNumber(actualizar);
+    console.log(actualizar);
+  };
+
+  useEffect(() => {
+    actNumber();
+  }, [cartstate]);
 
   return (
     <button className="nav-link">
       <Link to={"/cart"}>
         <i className="bi bi-cart"></i>
-        <span>{cartCounter}</span>
+        <span>{widgetNumber}</span>
       </Link>
     </button>
   );
